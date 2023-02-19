1dc1083e63d20ef4374d3ff2a21f426d41085cd2
Valery Makroguzov <valera.nedbai@gmail.ru>
2023-02-19 15:32:18 +0300

    add Project model

--- users/models.py
+++ users/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class Profile(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    repo = models.URLField(max_length=511, blank=True, default='')
+    owner = models.ForeignKey(Profile, related_name='projects', on_delete=models.CASCADE)
+    contributors = models.ManyToManyField(Profile,
+                                          related_name='contributors', blank=True)
+    """
+    Беру пример с github. 
+    Эти ребята те, кто работают над проектом.
+    """
+
+    def __str__(self):
+        return self.name
