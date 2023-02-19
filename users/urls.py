3b6ba74fd5c106ae608e3f0f5c5dc4bc08f167c3
Valery Makroguzov <valera.nedbai@gmail.ru>
2023-02-19 15:32:46 +0300

    add view set and update urls

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
+from .views import ProjectViewSet
 from .views import ReadOnlyProfileViewSet
 
 router = routers.SimpleRouter()
 router.register(r'', ReadOnlyProfileViewSet, basename='user')
+router.register(r'(?P<user_pk>[^/.]+)/project', ProjectViewSet, basename='project')
 
 urlpatterns = []
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from .models import Profile
-from .serializers import ReadOnlyUserSerializer
+from .serializers import Profile, ReadOnlyUserSerializer
+from .serializers import Project, ProjectSerializer
 
 
 class ReadOnlyProfileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ReadOnlyUserSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
